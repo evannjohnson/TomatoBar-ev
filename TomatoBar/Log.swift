a785bd58ab070bf40833c6d60b14b4f61168eca9
Evan Johnson <ev.jonnson@gmail.com>
2024-08-02 01:28:34 -0600

    make logHandle local to TBLogger methods
    When append() finishes, logHandle deallocates and closes its file descriptor.
    This causes an FSEvent to be generated for the file modification.

--- TomatoBar/Log.swift
+++ TomatoBar/Log.swift
@@ -32,7 +32,6 @@ private let lineEnd = "\n".data(using: .utf8)!
 internal let logger = TBLogger()
 
 class TBLogger {
-    private var logHandle: FileHandle?
     private let encoder = JSONEncoder()
 
     init() {
@@ -40,7 +39,8 @@ class TBLogger {
         encoder.dateEncodingStrategy = .secondsSince1970
     }
     
-    func openFileHandle() {
+    func openFileHandle() -> FileHandle? {
+        let logHandle: FileHandle?
         let fileManager = FileManager.default
         let logPath = fileManager
             .urls(for: .cachesDirectory, in: .userDomainMask)
@@ -52,19 +52,21 @@ class TBLogger {
             guard fileManager.createFile(atPath: logPath, contents: nil) else {
                 print("cannot create log file")
                 logHandle = nil
-                return
+                return nil
             }
         }
 
         logHandle = FileHandle(forUpdatingAtPath: logPath)
         guard logHandle != nil else {
             print("cannot open log file")
-            return
+            return nil
         }
+
+        return logHandle
     }
 
     func append(event: TBLogEvent) {
-        openFileHandle()
+        let logHandle = openFileHandle()
         guard let logHandle = logHandle else {
             return
         }
@@ -74,7 +76,6 @@ class TBLogger {
             try logHandle.seekToEnd()
             try logHandle.write(contentsOf: jsonData + lineEnd)
             try logHandle.synchronize()
-            try logHandle.close()
         } catch {
             print("cannot write to log file: \(error)")
         }
